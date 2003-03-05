9a807b7a1f2fac1920d7c92d4c0b5b40e1365f38
Jiří Šrain <jsrain@suse.cz>
2003-03-05 13:17:06 +0000

    added new testsuites for PPD agent
    svn path=/trunk/printer/; revision=9253

--- agent-ppd/testsuite/tests/runtest.sh
+++ agent-ppd/testsuite/tests/runtest.sh
@@ -5,5 +5,9 @@ unset Y2DEBUGGER
 
 (test -f /usr/share/YaST2/data/printer/models.equiv || cp ../../data/models.equiv /usr/share/YaST2/data/printer/;)
 
+export LANG="C"
+
 (./runag_ppd -l - $1 >$2) 2>&1 | fgrep -v " <0> " | grep -v "^$" | sed '-e s/^....-..-.. ..:..:.. [^)]*) //g' > $3
+
+rm ppd_*  2>/dev/null || exit 0
 
