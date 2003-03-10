8be738b06be7fc7173a0a7dbc2111f269958a639
Jiří Šrain <jsrain@suse.cz>
2003-03-10 14:51:54 +0000

    fixed lpdfilter's lprsetup calling (#24979) fixed testsuite during building problem
    svn path=/trunk/printer/; revision=9382

--- agent-ppd/testsuite/tests/runtest.sh
+++ agent-ppd/testsuite/tests/runtest.sh
@@ -3,8 +3,6 @@
 unset Y2DEBUG
 unset Y2DEBUGGER
 
-(test -f /usr/share/YaST2/data/printer/models.equiv || cp ../../data/models.equiv /usr/share/YaST2/data/printer/;)
-
 export LANG="C"
 
 (./runag_ppd -l - $1 >$2) 2>&1 | fgrep -v " <0> " | grep -v "^$" | sed '-e s/^....-..-.. ..:..:.. [^)]*) //g' > $3
